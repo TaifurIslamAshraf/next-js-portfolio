4851d1163d1cd1dce064ebc4cbc220384b514e25
MD Taifur <Taifurislamashraf@gmail.com>
2024-10-28 22:13:29 +0600

    [add] smooth scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,22 @@
+"use client";
+
 import Navbar from "@/components/navbar/Navbar";
 
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import ReactLenis from "@studio-freight/react-lenis";
 
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["200", "300", "400", "500", "600", "700", "800"],
 });
 
-export const metadata: Metadata = {
-  title: "Taifur | Portfolio",
-  description:
-    "full stack Taifur portfolio website with typescript and tailwind css",
-};
+// export const metadata: Metadata = {
+//   title: "Taifur | Portfolio",
+//   description:
+//     "full stack Taifur portfolio website with typescript and tailwind css",
+// };
 
 export default function RootLayout({
   children,
@@ -25,9 +28,11 @@ export default function RootLayout({
       <body
         className={`${poppins.className} bg-gradient-to-r from-slate-900 to-slate-700`}
       >
-        <Navbar />
+        <ReactLenis root>
+          <Navbar />
 
-        {children}
+          {children}
+        </ReactLenis>
       </body>
     </html>
   );
